8d97ae74eb1cbff1a46041a7ec2a2d12698b661d
noy1224 <167444444+mikami-haruhi@users.noreply.github.com>
2024-07-05 12:47:29 +0900

    コミット

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     console.log('自己紹介サイトへようこそ！');
 });
+
+$(window).scroll(function(){
+    //Header
+    if($(window).scrollTop()>= 100){
+       $('.header').addClass('fixed-top');
+    }
+    else {
+      $('.header').removeClass('fixed-top');
+    }
+    //Footer
+      var navpanel = $('.bottom-nav');
+      var panelHeight = navpanel.height() - 4;
+      var footerHeight = $('.footer').height();
+      if( $(document).height() - panelHeight - footerHeight <= $(window).scrollTop() + $(window).height() ) {
+        navpanel.removeClass('fixed-bottom');
+      } 
+    else {
+      navpanel.addClass('fixed-bottom');
+    }
+    
+  });
+  
